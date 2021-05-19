75e47e075f3173d128435b8e38787b93776d9e93
sk000f <mike@skaife.me>
2021-05-19 17:40:16 +0100

    environment config with dotenv

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/rs/zerolog v1.20.0
 	github.com/xanzy/go-gitlab v0.48.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/sk000f/metrix/adapters/api/rest"
+	"github.com/sk000f/metrix/adapters/ciserver/gitlab"
+	"github.com/sk000f/metrix/adapters/database/mongodb"
+	"github.com/sk000f/metrix/core/app"
+	"github.com/sk000f/metrix/core/ports"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	configureLogging()
+	cfg := setupConfig()
+	log.Info().Msg("Metrix starting ...")
+
+	ci := setupCIServer(cfg)
+
+	db := setupDatabase(cfg)
+
+	srv := setupAppCore(db, ci)
+
+	api := setupAPI(srv)
+
+	//err := srv.UpdateDeployments()
+	//if err != nil {
+	//	log.Error().Stack().Err(err).
+	//		Msg("main")
+	//}
+
+	log.Fatal().Err(http.ListenAndServe(":8080", api.Router))
+}
+
+func setupCIServer(cfg *Config) ports.CIServer {
+	g, err := gitlab.New(cfg.GitLabToken, cfg.GitLabURL)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	return g
+}
+
+func setupDatabase(cfg *Config) ports.Repository {
+	db, err := mongodb.New(cfg.MongoConn)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+
+	return db
+}
+
+func setupAppCore(db ports.Repository, ci ports.CIServer) ports.Service {
+	return app.New(db, ci)
+}
+
+func setupAPI(srv ports.Service) *rest.RestAPI {
+	return rest.New(srv)
+}
+
+func configureLogging() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+}
+
+func setupConfig() *Config {
+	cfg := new(Config)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Error().Stack().Err(err).
+			Msg("main.setupConfig")
+	}
+
+	cfg.GitLabURL = os.Getenv("METRIX_GITLAB_URL")
+	cfg.GitLabToken = os.Getenv("METRIX_GITLAB_TOKEN")
+	cfg.MongoConn = os.Getenv("METRIX_MONGO_CONN")
+
+	return cfg
+}
+
+type Config struct {
+	GitLabURL   string
+	GitLabToken string
+	MongoConn   string
+}
